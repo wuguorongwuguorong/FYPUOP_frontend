89670f4c4c4eab507db78898deebd1d60230daad
guorong wu <daniel.stillpixels@gmail.com>
2025-04-29 13:49:25 +0000

    Update on Register Page content

--- frontend_pos/src/RegisterPage.jsx
+++ frontend_pos/src/RegisterPage.jsx
@@ -3,8 +3,27 @@ import React from 'react';
 function RegisterPage() {
   return (
     <div className="container mt-5">
-      <h1>Welcome to Hungry Panda</h1>
-      <p>Register Now</p>
+      <h1>Register</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">User Name</label>
+          <input type="text" className="form-control" id="name" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input type="email" className="form-control" id="email" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" id="password" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+          <input type="password" className="form-control" id="confirmPassword" />
+        </div>
+        
+        <button type="submit" className="btn btn-primary">Register</button>
+      </form>
     </div>
   );
 }
