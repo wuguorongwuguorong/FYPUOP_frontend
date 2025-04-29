3676d22a3c4d41b5ae5258253af59fe765c0c2e9
guorong wu <daniel.stillpixels@gmail.com>
2025-04-29 12:59:19 +0000

    Navbar dropdown dynamic from bootstrap

--- frontend_pos/src/App.jsx
+++ frontend_pos/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
 import ProductCard from './ProductCard';
 import './styles.css';
 
 
 function App() {
 
+  const [isNavbarShowing, setNavbarShowing] = useState(false);
+
+  // Toggle the collapse state
+  const toggleNavbar = () => {
+    setNavbarShowing(!isNavbarShowing);
+  };
 
   return (
     <>
@@ -18,20 +24,22 @@ function App() {
 
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <a className="navbar-brand" href="#">Menu</a>
+          <a className="navbar-brand" href="#">E-Shop</a>
           <button
             className="navbar-toggler"
             type="button"
+            onClick={toggleNavbar}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className={`collapse navbar-collapse ${isNavbarShowing ? "show" : ""}`}
+            id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Products</a>
+                <a className="nav-link" href="#">Menu</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">About</a>
--- frontend_pos/src/ProductCard.jsx
+++ frontend_pos/src/ProductCard.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 
 const ProductCard = (props) => {
-  return (
-    <div className="card">
-      <img
-        src={props.image_url}
-        className="card-img-top"
-        alt={props.menu_item_name}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{props.menu_item_name}</h5>
-        <p className="card-text">${props.menu_item_price}</p>
-        <button className="btn btn-primary">
-          Add to Cart
-        </button>
-      </div>
-    </div>
-  );
+
+    const handleAddToCart = () => {
+        alert("Added to Cart!")
+    }
+    return (
+        <div className="card">
+            <img
+                src={props.image_url}
+                className="card-img-top"
+                alt={props.menu_item_name}
+            />
+            <div className="card-body">
+                <h5 className="card-title">{props.menu_item_name}</h5>
+                <p className="card-text">${props.menu_item_price}</p>
+                <button className="btn btn-primary" onClick={handleAddToCart}>
+                    Add to Cart
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default ProductCard;
